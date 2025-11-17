7be2d5dd352208311ef1a2caca72c56805028f5d
jinji7581 <con14560@ict.nitech.ac.jp>
2025-11-17 20:14:46 +0900

    webscket

---
+++ src/hooks/useWebSocket.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+import { connectionWebsocket } from "../lib/websocket";
+
+export const useWebSocket = (
+  url: string,
+  onMessage: (data: string) => void
+) => {
+  const socketRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    const socket = connectionWebsocket(url);
+    socketRef.current = socket;
+
+    socket.onmessage = (event) => {
+      onMessage(event.data);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, [url, onMessage]);
+};
